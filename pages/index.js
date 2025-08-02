970b6c18a888a6fe135ce75e22731538fa5f68e6
Kadasatish <kadasatish2022@gmail.com>
2025-08-02 19:59:31 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+export default function Home() {
+  const [val, setVal] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch('/api/value')
+        .then(r => r.json())
+        .then(d => setVal(d.stored || ""));
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <main>
+      <h1>Tasker Value: {val}</h1>
+    </main>
+  );
+}
